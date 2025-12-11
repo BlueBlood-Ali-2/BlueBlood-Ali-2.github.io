d698b48286aae219f20ff30c6712b613805f5059
Muhammad Ali <bc250206768mal@vu.edu.pk>
2025-12-11 16:46:17 +0500

    NEW

--- _next/static/_tvkjlEFxv_hKcH5bxH8Q/_ssgManifest.js
+++
@@ -1,2 +0,0 @@
-self.__SSG_MANIFEST = new Set(["\u002F", "\u002Fabout", "\u002Fcontact", "\u002Fhacks", "\u002Fprojects", "\u002Freminder", "\u002Fstack"]);
-self.__SSG_MANIFEST_CB && self.__SSG_MANIFEST_CB()
